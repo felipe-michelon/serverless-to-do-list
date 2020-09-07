f461b78901ba2d2e431ce7ef2407548ef8ad113b
Felipe Michelon <felipe.michelon@outlook.com>
2020-09-07 18:44:44 -0300

    Create function to delete a todo item

---
+++ src/handlers/delete.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+module.exports.delete = (event, context, callback) => {
+  const databaseConnection = event.databaseConnection || new AWS.DynamoDB.DocumentClient();
+  const databaseParams = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  databaseConnection.delete(databaseParams, (error) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: 500,
+        headers: { 'Content-Type': 'text/plain' },
+        body: "Couldn't delete the todo item",
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 204,
+      body: null,
+    };
+    callback(null, response);
+  });
+};
---
+++ test/handlers/delete.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const mochaPlugin = require('serverless-mocha-plugin');
+const sinon  = require('sinon');
+const AWS = require('aws-sdk');
+
+const expect = mochaPlugin.chai.expect;
+let wrapped = mochaPlugin.getWrapper('delete', '/src/handlers/delete.js', 'delete');
+
+describe('delete', () => {
+  const databseStub = sinon.createStubInstance(AWS.DynamoDB.DocumentClient);
+  const args = {
+    databaseConnection: databseStub,
+    pathParameters: { id: 123 },
+  };
+
+  describe('when making a request', () => {
+    let response;
+
+    describe('and the item is deleted from the database', () => {
+      before(async () => {
+        databseStub.delete.yields(false);
+        response = await wrapped.run(args);
+      })
+
+      it('should return a 204 http status', () => {
+        expect(response.statusCode).to.equal(204);
+      });
+
+      it('should return a null body', () => {
+        expect(response.body).to.equal(null);
+      });
+    });
+
+    describe('and there is a problem on the database', () => {
+      before(async () => {
+        databseStub.delete.yields(true);
+        response = await wrapped.run(args);
+      })
+
+      it('should return a 500 http status', () => {
+        expect(response.statusCode).to.equal(500);
+      });
+
+      it('should return the content type as plain text', () => {
+        expect(response.headers['Content-Type']).to.equal('text/plain');
+      });
+
+      it('should return a friendly error message', () => {
+        expect(response.body).to.equal("Couldn't delete the todo item");
+      });
+    });
+  });
+});
